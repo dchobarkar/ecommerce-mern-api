e446961d9233545759f609a4bea23fa4f699aeb4
Darshan Chobarkar <dchobarkar@gmail.com>
2024-03-14 21:05:15 +0530

    add mongodb models

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { default: mongoose } = require("mongoose");
 const dotenv = require("dotenv");
+
+const userRoute = require("./routes/user");
 const app = express();
 
 dotenv.config();
@@ -9,6 +11,9 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("DB connection successful"))
   .catch((error) => console.log(error));
+
+app.use(express.json());
+app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend servre is running");
