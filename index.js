192c2b7bcb4890b3f9dd70cdd291b8b410542256
Darshan Chobarkar <dchobarkar@gmail.com>
2024-03-14 21:41:02 +0530

    add crud operations

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const dotenv = require("dotenv");
 
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 const app = express();
 
 dotenv.config();
@@ -16,6 +19,9 @@ mongoose
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend servre is running");
